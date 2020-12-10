969a574ee783cbc4750b71b04c0373b920f0507d
Rakesh Kumar Roy <rakeshroy78000@gmail.com>
2020-12-10 17:17:54 +0530

    Updating Comment Part

--- ChangeProductPrice/Plugin/Product.php
+++ ChangeProductPrice/Plugin/Product.php
@@ -1,5 +1,13 @@
 <?php
-
+/*
+ * MageRakesh
+ * Copyright (C) 2020 MageRakesh <rakeshroy78000@gmail.com>
+ *
+ * @package MageRakesh_ChangeProductPrice
+ * @copyright Copyright (c) 2020 MageRakesh_Customer (https://github.com/MageRakesh)
+ * @license http://opensource.org/licenses/gpl-3.0.html GNU General Public License,version 3 (GPL-3.0)
+ * @author MageRakesh <rakeshroy78000@gmail.com>
+ */
 namespace MageRakesh\ChangeProductPrice\Plugin;
 
 class Product
--- ChangeProductPrice/registration.php
+++ ChangeProductPrice/registration.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * MageRakesh
+ * Copyright (C) 2020 MageRakesh <rakeshroy78000@gmail.com>
+ *
+ * @package MageRakesh_ChangeProductPrice
+ * @copyright Copyright (c) 2020 MageRakesh_Customer (https://github.com/MageRakesh)
+ * @license http://opensource.org/licenses/gpl-3.0.html GNU General Public License,version 3 (GPL-3.0)
+ * @author MageRakesh <rakeshroy78000@gmail.com>
+ */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'MageRakesh_ChangeProductPrice',
